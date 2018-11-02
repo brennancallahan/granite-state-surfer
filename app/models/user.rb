5c7c37ff72ff0b716c9f54eec543183986aebc98
brennancallahan <bren.cal@gmail.com>
2018-11-02 08:24:03 -0400

    added devise functionality, ensured passing tests, added css elements

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   validates :username, presence: true
-   validates :first_name, presence: true
-   validates :last_name, presence: true
+   # validates :username, presence: true
+   # validates :first_name, presence: true
+   # validates :last_name, presence: true
    validates :email, presence: true
    validates :encrypted_password, presence: true
 
